d0533c04284733be1ed1449ca8ff428a5bcb4a1a
sawantan <aarya.sawant@gmail.com>
2016-02-24 11:02:22 -0500

    added tests for add, subtract, multiply, and divide

--- spec/math_spec.js
+++ spec/math_spec.js
@@ -1,5 +1,22 @@
+// tell the spec where to find our code
+var math = require('../math');
+
 describe("A math program to do arithmetic", function() {
+ 
   it("can add two numbers", function() {
-    expect(add(3,5)).toBe(8);
+    expect(math.add(3,5)).toBe(8);
+  });
+
+  it ("can subtract two numbers", function () {
+  	expect(math.subtract(7,3)).toBe(4);
+  });
+
+  it ("can multiply two numbers", function() {
+  	expect(math.multiply(5,3)).toBe(15);
+  });
+
+  it ("can divide two numbers", function() {
+  	expect(math.divide(10,2)).toBe(5);
   });
 });
+
